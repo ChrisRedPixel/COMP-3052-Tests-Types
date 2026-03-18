9a87fa91db770e9e3d8632206174d435ae31132f
ChrisRedPixel <160031644+ChrisRedPixel@users.noreply.github.com>
2026-03-17 22:24:52 -0400

    autenticar_usuario added

--- app.py
+++ app.py
@@ -1,2 +1,40 @@
 def saludar(nombre):
     return f"Hola, {nombre}!"
+
+def autenticar_usuario(username, password):
+    usuarios = {
+        "admin": "1234",
+        "javier": "python123",
+        "estudiante": "abc123"
+    }
+
+    # Validar campos vacíos
+    if username == "" or password == "":
+        return {
+            "success": False,
+            "message": "Usuario y contraseña son requeridos",
+            "response_time_ms": 50
+        }
+
+    # Validar usuario existente
+    if username not in usuarios:
+        return {
+            "success": False,
+            "message": "Usuario no existe",
+            "response_time_ms": 70
+        }
+
+    # Validar contraseña
+    if usuarios[username] != password:
+        return {
+            "success": False,
+            "message": "Contraseña incorrecta",
+            "response_time_ms": 80
+        }
+
+    # Caso exitoso
+    return {
+        "success": True,
+        "message": "Acceso concedido",
+        "response_time_ms": 60
+    }
